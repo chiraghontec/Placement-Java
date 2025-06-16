788c90be0200e815f186d7c5ccda285c7f6e5061
Chirag DS <dcet2300097@reva.edu.in>
2025-06-16 09:44:53 +0530

    Loops with Swtich Case

--- Loops.java
+++ Loops.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
+
 public class Loops {
     static int i=0;
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++) {
-            System.out.println("The value of i in for loop is: " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a value for option (0-2): ");
+        int option = scanner.nextInt();
+        switch (option) {
+            case 0: 
+                for(int i = 0; i < 5; i++) {
+                    System.out.println("The value of i in for loop is: " + i);
+                }   
+                break;
+            case 1:
+                while(i < 5) {
+                    System.out.println("The value of i in while loop is: " + i);
+                    i++;
+                }
+                break;
+            case 2:
+                i = 0;
+                do {
+                    System.out.println("The value of i in do-while loop is: " + i);
+                    i++;
+                } while (i < 5);
+                default:
+                System.out.println("Enter a valid option!!");
+                break;
         }
-        while (i < 5) {
-            System.out.println("The value of i in while loop is: " + i);
-            i++;
-        }
-        do {
-            System.out.println("The value of i in do-while loop is: " + i);
-            i++;
-        } while (i < 10);
     }
 }
